5772a4a46fd7f3948f84d3396a2a31b128099713
Steven <stevendawn98@gmail.com>
2022-05-20 18:51:31 -0400

    finished functions lab

--- index.js
+++ index.js
@@ -27,5 +27,13 @@ function distanceTravelledInFeet(start, destination){
 function calculatesFarePrice(start, destination){
     if (distanceTravelledInFeet(start, destination) < 400) {
         return 0;
-    }
+    } else if (distanceTravelledInFeet(start, destination) > 400 
+        && distanceTravelledInFeet(start, destination) < 2000) {
+            return 2.56;
+        } else if (distanceTravelledInFeet(start, destination) > 2000
+            && distanceTravelledInFeet(start, destination) < 2500) {
+                return 25;
+            } else {
+                return 'cannot travel that far'
+            }
 }
