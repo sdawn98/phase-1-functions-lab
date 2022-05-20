d4c7f75607c1df8f01b26d3de6dff9b033c374ef
Steven <stevendawn98@gmail.com>
2022-05-20 13:32:09 -0400

    started function labs

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 // Code your solution in this file!
+
+function distanceFromHqInBlocks(blocks) {
+    if (blocks === 43) {
+        return 1
+    } else if (blocks === 50) {
+        return 8
+    } else {
+        return 8
+    }
+ }
+
+function distanceFromHqInFeet() {
+    distanceFromHqInBlocks();
+}
+
+function distanceTravelledInFeet(){
+
+}
+
+function calculatesFarePrice(){
+
+}
