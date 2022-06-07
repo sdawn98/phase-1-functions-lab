a2bab2e098de3354f133e8112775659c6a57a2a5
Steven <stevendawn98@gmail.com>
2022-06-07 18:40:28 -0400

    commit tree 17fd4f98e7ab606fca7fbe7e0b4a5bd604d18b6b

--- index.js
+++ index.js
@@ -1,39 +1,26 @@
-// Code your solution in this file!
-
 function distanceFromHqInBlocks(blocks) {
-    if (blocks === 43) {
-        return 1;
-    } else if (blocks === 50) {
-        return 8;
-    } else {
-        return 8;
-    }
- }
-
-function distanceFromHqInFeet(feet) {
-    if (feet === 43) {
-        return 264;
-    } else if (feet === 50) {
-        return 2112;
-    } else {
-        return 2112;
-    }
+    let hq = 42
+    return Math.abs(hq - blocks)
 }
 
-function distanceTravelledInFeet(start, destination){
-    return ((Math.abs(start - destination)) * 264)
+function distanceFromHqInFeet(blocks) {
+    return distanceFromHqInBlocks(blocks) * 264
 }
 
-function calculatesFarePrice(start, destination){
-    if (distanceTravelledInFeet(start, destination) < 400) {
+function distanceTravelledInFeet(start, destination) {
+    return Math.abs(start - destination) * 264
+}
+
+function calculatesFarePrice(start, destination) {
+    let feetTraveled = distanceTravelledInFeet(start, destination)
+    if (feetTraveled < 400) {
         return 0;
-    } else if (distanceTravelledInFeet(start, destination) > 400 
-        && distanceTravelledInFeet(start, destination) < 2000) {
-            return 2.56;
-        } else if (distanceTravelledInFeet(start, destination) > 2000
-            && distanceTravelledInFeet(start, destination) < 2500) {
-                return 25;
-            } else {
-                return 'cannot travel that far';
-            }
+    } else if (feetTraveled > 400 && feetTraveled < 2000) {
+        return (feetTraveled - 400) * 0.02
+    } else if (feetTraveled <= 2500) {
+        return 25;
+    } else {
+        return 'cannot travel that far'
+    }
 }
+
